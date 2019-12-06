a62efb8a2e449de3f1d76c330b01033dfaaaa3f1
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-12-06 10:27:37 -0600

    made custom hook useData and PlayerCard

---
+++ client/src/Components/Hooks/useData.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import axios from axios;
+
+//custom hook 
+
+export const useData = () => {
+// set state
+    const [data, setData] = useState([]);
+//fetch data with useEffect
+    useEffect(() => {
+        axios
+            .get('http://localhost:5000/api/players')
+            .then(response => {
+                setData(response.data);
+                console.log(response.data);
+            })
+            .catch(error => console.log('Your data must have been kicked out of the stadium', error))
+    }, [])
+
+    return[data];
+
+}
---
+++ client/src/Components/PlayerCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//player card
+
+export const PlayerCard = props => 
+    <div>
+        <h1>{props.name}</h1>
+        <div>
+            <span>{props.country} </span>
+            <span>{props.searches} </span>
+        </div>
+    </div>
+    
