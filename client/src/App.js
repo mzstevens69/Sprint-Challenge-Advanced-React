2bcc20f12f855d7b75303d82cebde972a161434b
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-12-06 12:47:14 -0600

    got 2 tests but couldnt figure out how to testById

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Women Soccer Players</h1>
-      <Display data={ data }/>
+      <Display data={ data } />
     </div>
   );
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 // integration test
-it('renders data to App', () => {
+it('App renders without crashing ', () => {
 //Does it render 
 const component =
   render(<App />);
   console.log(component)
 });
+test('headline rendering to card', () => {
+  const { getByText } = render(<App/>);
+  getByText(/players/i);
+  getByText(/women/i);
+});
+
