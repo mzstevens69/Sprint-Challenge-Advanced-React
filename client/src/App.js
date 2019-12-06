834afe2b20f6f3f3ad1b5e13490973100337e608
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-12-06 14:42:51 -0600

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import { useData } from './Components/Hooks/useData';
 import { Display } from './Components/Display';
 
+//class App extends React.Component {
+
 function App() {
+
+// state = { 
+ // data: []
+//}
   const [data] = useData();
+//render()
   return (
     <div className="App">
       <h1>Women Soccer Players</h1>
-      <Display data={ data } />
+      <Display data={ data } /> 
+      {/* Display data={this.state.data} */}
     </div>
   );
 }
